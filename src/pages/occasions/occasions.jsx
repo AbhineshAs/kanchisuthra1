3902089e48df7ff031acca844a5ecf5d3d4d5f47
AssociateSoftware <associatesoftwarewaxtech005@gmail.com>
2026-08-31 07:24:26 +0530

    rename file

--- src/pages/occasions/occasions.jsx
+++ src/pages/occasions/occasions.jsx
@@ -1,6 +1,6 @@
 import "./occasions.css";
 
-import occasionHero from "../../assets/images/DSC07413.JPG";
+import occasionHero from "../../assets/images/DSC07413.jpg";
 import muhurtaimg from "../../assets/images/DSC00706.JPG";
 import praveshimg from "../../assets/images/DSC00716.JPG";
 import nithyaimg from "../../assets/images/DSC00745.JPG";
